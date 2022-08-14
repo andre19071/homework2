9a823d47d2156d7f32a9638c1efeb2271426c6dc
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-14 17:24:34 +0700

    task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -3,3 +3,17 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int digitIndex = 2;
+
+if(number > 100 )
+{
+    int digit = number.ToString()[digitIndex]-'0';
+    Console.WriteLine("Третья цифра  " + digit);
+}
+else
+{
+    Console.WriteLine($"Введенном числе {number} нет третьей цифры");
+}

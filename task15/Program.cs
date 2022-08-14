4457705b7f6fced17ec6f0aea7f9af98d3dcb154
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-14 18:42:57 +0700

    task15

--- task15/Program.cs
+++ task15/Program.cs
@@ -3,3 +3,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите порядковый номер дня недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<1 || number>7)
+{
+    Console.WriteLine("Введенная цифра несоответствует количеству дней в неделе");
+}
+else if (number>5)
+{
+   Console.WriteLine("Выходной день");
+}
+else if (number<=5)
+{
+    Console.WriteLine("Рабочий день");
+}
